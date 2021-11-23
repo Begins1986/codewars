7d4d374d1deff389c9136f984c497e870b8c61c8
Begin Sergey <begins.1986@mail.ru>
2021-11-23 21:01:41 +0300

    add task

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 function potatoes(p0, w0, p1) {
     return Math.floor(w0 * (100 - p0) / (100 - p1))
  }
+
+//  Breaking chocolate problem
+function breakChocolate(n,m) {
+    if (m<=0 || n<=0){
+      return 0;
+    }else{
+      return n*m-1;
+    }   
+  }
+
+//   Drink about
+function peopleWithAgeDrink(old) {
+    if (old<14){
+      return "drink toddy"
+    } else if(old>=14 && old<18){
+      return "drink coke"
+    } else if(old>=18 && old<21){
+      return "drink beer"
+    } else {
+      return 'drink whisky';
+    }
+  };
