d87ec0e3149b0c0056d9143c90ca3fc4d9b08ba7
Begin Sergey <begins.1986@mail.ru>
2021-12-10 23:30:39 +0300

    add kata

--- script.js
+++ script.js
@@ -105,3 +105,13 @@ function spinningRings(innerMax, outerMax) {
   }
 return i;
 };
+
+
+// String repeat
+function repeatStr (n, s) {
+  let str = '';
+  for(let i=0;i<n;i++){
+    str = str + s;  
+  }
+  return str;
+}
