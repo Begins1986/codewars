25762bdfa975a1e892834862fbd5539f2048f721
Begin Sergey <begins.1986@mail.ru>
2021-12-19 15:37:44 +0300

    added kata vower remover

--- script.js
+++ script.js
@@ -137,3 +137,16 @@ function finalGrade (exam, projects) {
   }
 }
 
+
+// Vowel remover
+function shortcut(str) {             
+  let newString = "";                            
+  for (let i = 0; i < str.length; i++)  
+{           
+    if (str[i] != "a" && str[i] != "e" && str[i] != "i" && str[i] != "o" && str[i] != "u")                                   
+     {           
+     newString += str[i];         
+    };   
+  };   
+  return newString;   
+};
