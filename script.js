5c2c8471575c9f59e1418430cb8f2d3e7da0a3ee
Begin Sergey <begins.1986@mail.ru>
2021-12-20 23:17:51 +0300

    added kata BMI

--- script.js
+++ script.js
@@ -150,3 +150,19 @@ function shortcut(str) {
   };   
   return newString;   
 };
+
+
+// Calculate BMI
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+  switch(true){
+    case bmi <= 18.5:
+      return "Underweight";
+    case bmi <= 25.0:
+      return "Normal";
+    case bmi <= 30.0:
+      return "Overweight";
+    case bmi > 30:
+      return "Obese";
+   }
+ }
