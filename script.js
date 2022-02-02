1ee14fd90063d3f1b6718af5c11a3711429ae76d
Begin Sergey <begins.1986@mail.ru>
2022-02-02 23:35:20 +0300

    added kata stray number

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
 // Drying Potatoes
 function potatoes(p0, w0, p1) {
-    return Math.floor(w0 * (100 - p0) / (100 - p1))
- }
+  return Math.floor((w0 * (100 - p0)) / (100 - p1));
+}
 
 //  Breaking chocolate problem
-function breakChocolate(n,m) {
-    if (m<=0 || n<=0){
-      return 0;
-    }else{
-      return n*m-1;
-    }   
+function breakChocolate(n, m) {
+  if (m <= 0 || n <= 0) {
+    return 0;
+  } else {
+    return n * m - 1;
   }
+}
 
 //   Drink about
 function peopleWithAgeDrink(old) {
-    if (old<14){
-      return "drink toddy"
-    } else if(old>=14 && old<18){
-      return "drink coke"
-    } else if(old>=18 && old<21){
-      return "drink beer"
-    } else {
-      return 'drink whisky';
-    }
-};
+  if (old < 14) {
+    return "drink toddy";
+  } else if (old >= 14 && old < 18) {
+    return "drink coke";
+  } else if (old >= 18 && old < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
 
 // Parse nice int from char problem
-function getAge(inputString){
+function getAge(inputString) {
   return parseInt(inputString[0]);
 }
 
 // Reversed Message
 function reverseMessage(str) {
-  str=str.split(` `).map(v=>v.split('').reverse().join``).join` `;
-  str=str.split(` `).map(v=>v.slice(0,1).toUpperCase()+v.slice(1).toLowerCase()).reverse().join` `;
+  str = str.split(` `).map((v) => v.split("").reverse().join``).join` `;
+  str = str
+    .split(` `)
+    .map((v) => v.slice(0, 1).toUpperCase() + v.slice(1).toLowerCase())
+    .reverse().join` `;
   return str;
 }
 
 // Narcissistic Numbers
-function isNarcissistic(n){
-  return n === +n.toString().split('').reduce((s, v, i, arr) => s + Math.pow(v, arr.length),0)
+function isNarcissistic(n) {
+  return (
+    n ===
+    +n
+      .toString()
+      .split("")
+      .reduce((s, v, i, arr) => s + Math.pow(v, arr.length), 0)
+  );
 }
 
 // Boiled Eggs
@@ -48,114 +57,118 @@ function cookingTime(eggs) {
 }
 
 // Count the divisors of a number
-function getDivisorsCnt(n){
-  let i, sum=0;
-for(i=0; i<=n; i++){
-  if (n%i==0){
-    sum++;
+function getDivisorsCnt(n) {
+  let i,
+    sum = 0;
+  for (i = 0; i <= n; i++) {
+    if (n % i == 0) {
+      sum++;
+    }
   }
-}
-return sum;
+  return sum;
 }
 
 // I love you, a little , a lot, passionately ... not at all
 function howMuchILoveYou(nbPetals) {
-  if (nbPetals%6==1){
+  if (nbPetals % 6 == 1) {
     return "I love you";
-  } else if (nbPetals%6==2){
+  } else if (nbPetals % 6 == 2) {
     return "a little";
-  } else if (nbPetals%6==3){
+  } else if (nbPetals % 6 == 3) {
     return "a lot";
-  } else if (nbPetals%6==4){
+  } else if (nbPetals % 6 == 4) {
     return "passionately";
-  } else if (nbPetals%6==5){
+  } else if (nbPetals % 6 == 5) {
     return "madly";
-  } else if (nbPetals%6==0){
+  } else if (nbPetals % 6 == 0) {
     return "not at all";
   }
 }
- 
+
 // Is this a triangle?
-function isTriangle(a,b,c){ 
-  if(a+b>c && a+c>b && b+c>a){
-  return true;
-} else{
-  return false;
-}   
+function isTriangle(a, b, c) {
+  if (a + b > c && a + c > b && b + c > a) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Correct the mistakes of the character recognition software
-function correct(string){
-  return string.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S');
+function correct(string) {
+  return string.replace(/0/g, "O").replace(/1/g, "I").replace(/5/g, "S");
 }
 
 // Reverse a Number
 function reverseNumber(n) {
-  if (n>=0){
-   return +Math.abs(n).toString().split('').reverse().join(''); 
+  if (n >= 0) {
+    return +Math.abs(n).toString().split("").reverse().join("");
   } else {
-    return +Math.abs(n).toString().split('').reverse().join('')*(-1);
-  } 
+    return +Math.abs(n).toString().split("").reverse().join("") * -1;
+  }
 }
 
 // Spinning Rings
 function spinningRings(innerMax, outerMax) {
-  for(var i = 1; innerMax - ((i - 1) % (innerMax + 1)) !== i % (outerMax + 1); i++) {
-    
-  }
-return i;
-};
-
+  for (
+    var i = 1;
+    innerMax - ((i - 1) % (innerMax + 1)) !== i % (outerMax + 1);
+    i++
+  ) {}
+  return i;
+}
 
 // String repeat
-function repeatStr (n, s) {
-  let str = '';
-  for(let i=0;i<n;i++){
-    str = str + s;  
+function repeatStr(n, s) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str = str + s;
   }
   return str;
 }
 
 // Return Negativeg
 function makeNegative(num) {
-  if (num<0){
+  if (num < 0) {
     return num;
-  } else return num*(-1);
+  } else return num * -1;
 }
 
 //Student's Final Grade
-function finalGrade (exam, projects) {
-  console.log (exam, projects)
-  if ((projects > 10)||(exam > 90)){
+function finalGrade(exam, projects) {
+  console.log(exam, projects);
+  if (projects > 10 || exam > 90) {
     return 100;
-  } else if ((exam > 75) && (projects >= 5)){
+  } else if (exam > 75 && projects >= 5) {
     return 90;
-  } else   if ((exam > 50) && (projects >= 2)){
+  } else if (exam > 50 && projects >= 2) {
     return 75;
-  } else{
+  } else {
     return 0;
   }
 }
 
-
 // Vowel remover
-function shortcut(str) {             
-  let newString = "";                            
-  for (let i = 0; i < str.length; i++)  
-{           
-    if (str[i] != "a" && str[i] != "e" && str[i] != "i" && str[i] != "o" && str[i] != "u")                                   
-     {           
-     newString += str[i];         
-    };   
-  };   
-  return newString;   
-};
-
+function shortcut(str) {
+  let newString = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] != "a" &&
+      str[i] != "e" &&
+      str[i] != "i" &&
+      str[i] != "o" &&
+      str[i] != "u"
+    ) {
+      newString += str[i];
+    }
+  }
+  return newString;
+}
 
 // Calculate BMI
 function bmi(weight, height) {
   let bmi = weight / (height * height);
-  switch(true){
+  switch (true) {
     case bmi <= 18.5:
       return "Underweight";
     case bmi <= 25.0:
@@ -164,136 +177,148 @@ function bmi(weight, height) {
       return "Overweight";
     case bmi > 30:
       return "Obese";
-   }
- }
-
+  }
+}
 
 //  Beginner Series #1 School Paperwork
- function paperwork(n, m) {
-  return n > 0 && m > 0 ? n * m : 0
+function paperwork(n, m) {
+  return n > 0 && m > 0 ? n * m : 0;
 }
 
 // Is he gonna survive?
-function hero(bullets, dragons){
-  return ((bullets/2)>=dragons) ? true : false;
+function hero(bullets, dragons) {
+  return bullets / 2 >= dragons ? true : false;
 }
 
 // Find the Remainder
-function remainder(a, b){
-  return a>b ? a%b : b%a
+function remainder(a, b) {
+  return a > b ? a % b : b % a;
 }
 
-
 // Convert a Boolean to a String
-function booleanToString(b){
+function booleanToString(b) {
   return String(b);
- }
+}
 
 //  Abbreviate a Two Word Name
- function abbrevName(name){
-  return name.toUpperCase().split(' ').map(el=>el.charAt(0)).join('.');
+function abbrevName(name) {
+  return name
+    .toUpperCase()
+    .split(" ")
+    .map((el) => el.charAt(0))
+    .join(".");
 }
 
 // Two Oldest Ages
-function twoOldestAges(ages){
-  return ages.sort((a, b) => a - b).slice(-2)
+function twoOldestAges(ages) {
+  return ages.sort((a, b) => a - b).slice(-2);
 }
 
 // Thinkful - Logic Drills: Traffic light
 function updateLight(current) {
-  if (current == 'green'){
-   return 'yellow'
- } else if (current == 'yellow'){
-   return 'red'
- } else if (current == 'red'){
-   return 'green'
- }
+  if (current == "green") {
+    return "yellow";
+  } else if (current == "yellow") {
+    return "red";
+  } else if (current == "red") {
+    return "green";
+  }
 }
-
 
 // Exclamation marks series #2: Remove all exclamation marks from the end of sentence
-function remove (string) {
-  let arr=string.split('');
-  for(let i=arr.length-1; i>0; i--){
-    if(arr[i]=='!'){
-     arr.pop()
-     } else {
-       break
-     }
-  }  
-  console.log(arr)
-  return arr.join('');
+function remove(string) {
+  let arr = string.split("");
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i] == "!") {
+      arr.pop();
+    } else {
+      break;
+    }
+  }
+  console.log(arr);
+  return arr.join("");
 }
-
-
 
 // Grasshopper - Check for factor
-function checkForFactor (base, factor) {
-  return base%factor==0?true:false
+function checkForFactor(base, factor) {
+  return base % factor == 0 ? true : false;
 }
-
 
 // Thinkful - Number Drills: Congo warehouses
 function boxCapacity(length, width, height) {
-  return Math.trunc(length/(16/12))*Math.trunc(width/(16/12))*Math.trunc(height/(16/12)); //code here
+  return (
+    Math.trunc(length / (16 / 12)) *
+    Math.trunc(width / (16 / 12)) *
+    Math.trunc(height / (16 / 12))
+  ); //code here
 }
 
 // Holiday VIII - Duty Free
-function dutyFree(normPrice, discount, hol){
-  return Math.trunc(hol/(normPrice*(discount/100)))
-  }
+function dutyFree(normPrice, discount, hol) {
+  return Math.trunc(hol / (normPrice * (discount / 100)));
+}
 
-  // Switch it Up!
-  function switchItUp(number){
-    switch(number) {
-      case 0:
-        return 'Zero';
-      case 1:
-        return 'One';
-      case 2:
-        return 'Two';
-      case 3:
-        return 'Three';
-      case 4:
-        return  'Four';
-      case 5:
-        return 'Five';
-      case 6:
-        return 'Six';
-      case 7:
-        return 'Seven';
-      case 8:
-        return 'Eight';
-      case 9:
-        return 'Nine';
-      default:
-        return 'Unknown number';
-    }
+// Switch it Up!
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+    default:
+      return "Unknown number";
   }
+}
 
-  // Volume of a Cuboid
-  class Kata {
-    static getVolumeOfCuboid(length, width, height) {
-      return length*width*height
-    }
+// Volume of a Cuboid
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
   }
+}
 
-  // JavaScript Array Filter
-  function getEvenNumbers(numbersArray){
-    let result = numbersArray.filter(number => number%2==0)
-     return result 
-  }
+// JavaScript Array Filter
+function getEvenNumbers(numbersArray) {
+  let result = numbersArray.filter((number) => number % 2 == 0);
+  return result;
+}
 
-  // Sum of two lowest positive integers
-  function sumTwoSmallestNumbers(numbers) { 
-    let arr = numbers.sort(function(a, b) {
+// Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {
+  let arr = numbers.sort(function (a, b) {
     return a - b;
   });
-    console.log(arr)
-    return arr[0]+arr[1]
-  }
+  console.log(arr);
+  return arr[0] + arr[1];
+}
 
-  // Sum of Minimums!
-  function sumOfMinimums(arr) {
-    return arr.map(v=>Math.min(...v)).reduce((a,b)=>a+b,0)
+// Sum of Minimums!
+function sumOfMinimums(arr) {
+  return arr.map((v) => Math.min(...v)).reduce((a, b) => a + b, 0);
+}
+
+// Find the stray number
+function stray(numbers) {
+  const arr = numbers.sort();
+  if (arr[0] == arr[1]) {
+    return arr[arr.length - 1];
+  } else {
+    return arr[0];
   }
+}
