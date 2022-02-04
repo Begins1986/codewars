d8607d36b97d5d7e1ea72c2c25fbeddfd5138170
Begin Sergey <begins.1986@mail.ru>
2022-02-04 23:53:16 +0300

    added kata charter counter

--- script.js
+++ script.js
@@ -322,3 +322,13 @@ function stray(numbers) {
     return arr[0];
   }
 }
+
+// Character Counter
+function validateWord(s){
+  var counts = {};
+  for(var c of s.toLowerCase()){
+    if(counts[c] === undefined){counts[c] = 0;}
+    counts[c]++;
+  }
+  return Math.max(...Object.values(counts)) === Math.min(...Object.values(counts));
+}
