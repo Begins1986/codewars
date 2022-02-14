898b80abbdf867d62183320f98166f2f4d55fdea
Begin Sergey <begins.1986@mail.ru>
2022-02-14 23:42:52 +0300

    add kata slope of a line

--- script.js
+++ script.js
@@ -370,3 +370,14 @@ function lastDigit(n, d) {
        return (""+n).split("").map(Number).slice(-d)
     }
   }
+
+
+  // Slope of a Line 
+  function getSlope(p1, p2) {
+    console.log(p1, p2)
+   if(p2[0]==p1[0]&&p2[1]==p1[1]||p2[0]==p1[0]){
+      return null
+    } else {
+      return (p2[1] - p1[1]) / (p2[0] - p1[0])
+    }
+  }
