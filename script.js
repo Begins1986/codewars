a25d9a133faaab496199ad55cfb917717be3e3b0
Begin Sergey <begins.1986@mail.ru>
2022-04-22 23:08:47 +0300

    add kata bin to decimal

--- script.js
+++ script.js
@@ -520,3 +520,8 @@ function gimme (triplet) {
 function openOrSenior(data){
   return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
 }
+
+// Bin to Decimal
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
