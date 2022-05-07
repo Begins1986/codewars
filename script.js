3c8a0b22df8e89bf10006e8c03ca00cb599dcf42
Begin Sergey <begins.1986@mail.ru>
2022-05-07 23:34:38 +0300

    added kata not FIVE

--- script.js
+++ script.js
@@ -547,3 +547,15 @@ function vowelIndices(word){
    }
    return arr
  }
+
+
+//  Don't give me five!
+ function dontGiveMeFive(start, end) {
+  let count = 0
+  for (let i = start; i <= end; i++) {
+    if (!/5/.test(i)) {
+      count++
+    }
+  }
+  return count
+}
