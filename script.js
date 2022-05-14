cc61d7de36b7e02d55ffb1957fa4ecd3d394108b
Begin Sergey <begins.1986@mail.ru>
2022-05-14 23:47:48 +0300

    add kata going to the cinema

--- script.js
+++ script.js
@@ -576,3 +576,15 @@ function between(a, b) {
   }
   return arr
 }
+
+
+// Going to the cinema
+function movie(card, ticket, perc) {
+  let costA = n = 0,
+         costB = card;
+     while (Math.ceil(costB) >= costA) {
+         costA += ticket;
+         costB += ticket * Math.pow(perc,++n);
+     }
+     return n;
+ };
