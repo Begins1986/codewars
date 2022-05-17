c300156185e35da0b1341f17c189d6dfe44127a5
Begin Sergey <begins.1986@mail.ru>
2022-05-17 22:44:08 +0300

    add kata find all pairs

--- script.js
+++ script.js
@@ -588,3 +588,18 @@ function movie(card, ticket, perc) {
      }
      return n;
  };
+
+
+//  Find all pairs
+ function duplicates(array){
+array=array.slice().sort();
+  let count = 0
+  for (let i=0; i<array.length;i++){
+      if (array[i]===array[i+1]){
+        count++
+        array[i]=''
+        array[i+1]=''
+      }
+    }
+  return count
+}
