fa9cfdb1282ff3f290b95c3de7d94cb6c03da1dd
Begin Sergey <begins.1986@mail.ru>
2022-06-01 23:19:20 +0300

    add kata 101 dolmatians

--- script.js
+++ script.js
@@ -636,3 +636,18 @@ function sumOfDifferences(arr) {
     return arr1[0]-arr1[arr.length-1]
   }
 }
+
+
+// 101 Dalmatians - squash the bugs, not the dogs!
+function howManyDalmatians(number){
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  if (number <= 10) {
+    return dogs[0]
+  } else if (number <= 50) {
+    return dogs[1]
+  } else if (number === 101) {
+    return dogs[3]
+  } else {
+    return dogs[2] 
+  }
+}
